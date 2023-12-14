3f7b5d3800978766c404194a29668737593ae6e5
Andrey Ryazanov <ark2635@gmail.com>
2023-12-14 14:24:09 +0300

    upd urls

--- main/urls.py
+++ main/urls.py
@@ -2,13 +2,12 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 
-from main.views import home, contacts, about, write_data_to_json
+from main.views import home, contacts, write_data_to_json
 
 app_name = 'main'
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('about/', about, name='about'),
     path('write_data_to_json/', write_data_to_json, name='write_data_to_json'),
 ]
