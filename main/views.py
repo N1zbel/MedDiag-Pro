9eedf5eb3a18012e7a77f9ba88a74808f0a8aeac
Andrey Ryazanov <ark2635@gmail.com>
2023-12-14 13:33:40 +0300

    add views for main app

---
+++ main/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from services.models import Service, ServiceCategory
+
+
+def home(request):
+    context = {
+        'title': 'Домашняя страница'
+    }
+    return render(request, 'main/home.html', context=context)
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name}, {phone}, {message}')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'main/contacts.html', context)
+
+
+def write_data_to_json(request):
+    services = list(Service.objects.values())
+    categories = list(ServiceCategory.objects.values())
+
+    data = {"services": services, "categories": categories}
+
+    json_file_path = "main/data.json"
+    with open(json_file_path, 'w', encoding='UTF-8') as formatted_file:
+        json.dump(data, formatted_file, ensure_ascii=False, indent=2)
+
+    return JsonResponse({"status": "success"})
