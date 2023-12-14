8df7eb01af1dd2d2fb65b17af73ab7e180f0a23f
Andrey Ryazanov <ark2635@gmail.com>
2023-12-14 14:22:13 +0300

    add about_us app

--- about_us/apps.py
+++ about_us/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BlogConfig(AppConfig):
+class AboutUsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog'
+    name = 'about_us'
