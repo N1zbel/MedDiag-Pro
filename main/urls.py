6525d5dd666e4b4759d8251329ed8b3b6efa5c8d
Andrey Ryazanov <ark2635@gmail.com>
2023-12-14 13:33:48 +0300

    add urls for main app

--- main/urls.py
+++ main/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 
-from main.views import home, contacts, about, services
+from main.views import home, contacts, about, write_data_to_json
 
-app_name = 'catalog'
+app_name = 'main'
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
     path('about/', about, name='about'),
-    path('services/', services, name='services'),
+    path('write_data_to_json/', write_data_to_json, name='write_data_to_json'),
 ]
